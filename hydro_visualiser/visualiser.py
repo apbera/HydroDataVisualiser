8cdd92324cf4ec9bcd3b84734182042ed5cfa1ff
Kacior <maciek.kazalski@gmail.com>
2022-09-06 22:46:15 +0200

    added pin and widget generation to lib

--- hydro_visualiser/visualiser.py
+++ hydro_visualiser/visualiser.py
@@ -37,3 +37,42 @@ def create_split_map(base,left_layer_source, right_layer_source, style=None, zoo
     result.add_control(opacity_control_right)
 
     return result
+
+#pinsArray format: [{x1, y2}, {x2, y2}...]
+def draw_chart(charts):
+    data1 = pd.DataFrame(np.random.normal(size = 50))
+    charts.clear_output()
+    sns.set_theme()
+    with charts:
+        fig1, axes1 = plt.subplots()
+        data1.hist(ax = axes1)
+        plt.show(fig1)    
+
+def handle_click(number, out, charts):
+    def update_widget(**kwargs):
+        out.clear_output()
+        out.append_stdout("Example station nr: " + str(number) + "data")
+        out.append_stdout("\n")
+        draw_chart(charts)
+    return update_widget
+
+def addPinsAndWidgets(base, pinsArray):
+    pins=[]
+    out = widgets.Output(layout={'border': '1px solid black'})
+    charts = widgets.Output(layout={'border': '1px solid black'})
+    children=[]
+    tab = widgets.Tab(children = [out, charts])
+    for i in range(len(children)):
+        tab.set_title(i, 'a')
+    widget_tab = WidgetControl(widget = tab, position="bottomleft")
+    base.add(widget_tab)
+
+    for i in range(1, len(pinsArray)):
+        new_marker = Marker(location = (pinsArray[i][0], pinsArray[i][1]), draggable=False)
+        new_marker.on_click(handle_click(i, out, charts))
+        pins.append(new_marker)
+    marker_cluster = MarkerCluster(
+        markers=pins
+    )
+    base.add_layer(marker_cluster)
+    return base
