7c208b53e68e9a5fba792799a30340ff90623e88
Kacior <maciek.kazalski@gmail.com>
2022-09-06 21:09:09 +0200

    asd

--- hydro_visualiser/visualiser.py
+++ hydro_visualiser/visualiser.py
@@ -7,7 +7,6 @@ def hydro_map():
     m = leafmap.Map(center=(40, -100), zoom=4)
     return m
 
-
 def visualise_geojson(path):
     m = leafmap.Map(layers_control=True)
     m.add_geojson(path, layer_name='Geojson')
